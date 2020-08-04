23c0c37c022c108b82b925c4ceb060e0799f2344
JpabloMF <juanpablo45671@gmail.com>
2020-08-03 21:35:27 -0500

    visual improvements

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
+import ContactPage from '../pages/contact';
+
 export default () => (
   <StaticQuery
     query={graphql`
@@ -14,15 +16,23 @@ export default () => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <div className="hero-header">
-        <div className="headline">{data.site.siteMetadata.home.title}</div>
-        <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
+        <div className="headline-title">¡Hello!</div>
+        <div className="headline site-titles-blue">
+          {data.site.siteMetadata.home.title}
+        </div>
+        <div
+          className="primary-content"
+          dangerouslySetInnerHTML={{
+            __html: data.site.siteMetadata.home.description,
+          }}
         />
-        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+        {/* <Link to="/contact" className="button -primary">
+          Get in touch &rarr;
+        </Link> */}
+        <ContactPage/>
       </div>
     )}
   />
-)
+);
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,59 +1,48 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import React from 'react';
+import Helmet from 'react-helmet';
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = () => {
   return (
-    <Layout>
+    <>
       <Helmet>
-        <title>Contact — {site.siteMetadata.title}</title>
-        <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
+        <title>Contact — MFCoder</title>
+        <meta name="description" content="Contact page of MFCoder" />
       </Helmet>
       <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
+        <div className="post-thumbnail">
+          <h1 className="post-title">Subscribe to our newsletter!</h1>
+          <p>
+            I'll never use your email for anything different than notifying you
+            about new articles.
+          </p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
+          <form
+            className="form-container"
+            action="https://sendmail.w3layouts.com/SubmitContactForm"
+            method="post"
+          >
             <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+              <label htmlFor="w3lSender">Enter your email</label>
+              <input
+                type="email"
+                name="w3lSender"
+                id="w3lSender"
+                placeholder="example@mail.com"
+              />
             </div>
             <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
+              <input
+                type="submit"
+                className="button -primary"
+                style={{ marginRight: 0, cursor: 'pointer' }}
+                value="Sign me up!"
+              />
             </div>
           </form>
         </div>
       </div>
-    </Layout>
-  )
-}
-export default ContactPage
-export const pageQuery = graphql`
-  query ContactPageQuery{
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-  }
-`
+    </>
+  );
+};
+export default ContactPage;
